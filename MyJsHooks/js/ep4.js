5632677e365a441b90935ac107de85b75b66affd
anand-amj <anandjadhav.amj@gmail.com>
2023-09-29 12:25:19 +0530

    variable example

---
+++ MyJsHooks/js/ep4.js
@@ -0,0 +1,7 @@
+const contactId = 1234;
+let conatactName = "Anand";
+var contactCountry = "Bharat";
+contactCity = "pune"
+contactName = "JJ";
+contactCountry = "USA";
+console.table([contactId, conatactName, contactCountry, contactCity])
